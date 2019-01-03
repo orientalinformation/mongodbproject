71fc34f269d542f886d0c3a499d076a00ec2858c
Trieu Nguyen <baotrieunguyen@gmail.com>
2019-01-03 09:31:39 +0700

    add layout auth

---
+++ resources/views/Backend/Auth/signin.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Twitter -->
+    <meta name="twitter:site" content="@themepixels">
+    <meta name="twitter:creator" content="@themepixels">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Bracket">
+    <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="twitter:image" content="http://themepixels.me/bracket/img/bracket-social.png">
+
+    <!-- Facebook -->
+    <meta property="og:url" content="http://themepixels.me/bracket">
+    <meta property="og:title" content="{{ __('left-panel.logoName') }}">
+    <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
+
+    <meta property="og:image" content="http://themepixels.me/bracket/img/bracket-social.png">
+    <meta property="og:image:secure_url" content="http://themepixels.me/bracket/img/bracket-social.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="600">
+
+    <!-- Meta -->
+    <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="author" content="ThemePixels">
+
+    <title>Bracket Responsive Bootstrap 4 Admin Template</title>
+
+    <!-- vendor css -->
+    <link href="{{ asset('/assets/lib/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ asset('/assets/lib/Ionicons/css/ionicons.css') }}" rel="stylesheet">
+
+    <!-- Bracket CSS -->
+    <link rel="stylesheet" href="{{ asset('/assets/css/bracket.css') }}">
+
+</head>
+
+<body>
+
+<div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
+
+    <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white rounded shadow-base">
+        <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> {{ __('left-panel.logoName') }} <span class="tx-normal">]</span></div>
+        <div class="tx-center mg-b-60"></div>
+
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Enter your username">
+        </div><!-- form-group -->
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Enter your password">
+            <a href="" class="tx-info tx-12 d-block mg-t-10">Forgot password?</a>
+        </div><!-- form-group -->
+        <button type="submit" class="btn btn-info btn-block">Sign In</button>
+
+        <div class="mg-t-60 tx-center">Not yet a member? <a href="" class="tx-info">Sign Up</a></div>
+    </div><!-- login-wrapper -->
+</div><!-- d-flex -->
+
+<script src="{{ asset('/assets/lib/jquery/jquery.js') }}"></script>
+<script src="{{ asset('/assets/lib/popper.js/popper.js') }}"></script>
+<script src="{{ asset('/assets/lib/bootstrap/bootstrap.js') }}"></script>
+
+</body>
+</html>
