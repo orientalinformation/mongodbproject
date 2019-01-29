1abafd3e9535312675a15206e70077b4c2132fe9
DFM_TRIEU_NGUYEN <baotrieunguyen@gmail.com>
2019-01-29 15:33:47 +0700

    upsource

--- routes/web.php
+++ routes/web.php
@@ -18,202 +18,202 @@ Route::get('/', function () {
 
 //===========BACKEND==========
 
-Route::group(['middleware' => ['auth']], function () {
+Route::prefix('admin/')->group(function () {
 
-    Route::prefix('admin/')->group(function () {
+    //default
+    Route::get('/', 'Backend\DashboardController@index');
 
-        //default
-        Route::get('/', 'Backend\DashboardController@index');
+    //Login routes
+    Route::get('login', ['uses' => 'Backend\AuthController@login',     'as' => 'login']);
+    Route::post('login', ['uses' => 'Backend\AuthController@postLogin', 'as' => 'postlogin']);
+    Route::get('logout', ['uses' => 'Backend\AuthController@logout', 'as' => 'logout']);
 
-        //Login routes
-        Route::get('login', ['uses' => 'Backend\AuthController@login',     'as' => 'login']);
-        Route::post('login', ['uses' => 'Backend\AuthController@postLogin', 'as' => 'postlogin']);
-        Route::get('logout', ['uses' => 'Backend\AuthController@logout', 'as' => 'logout']);
+    //Password reset routes
+    Route::get('forgot-password', ['uses' => 'Backend\AuthController@showForgotForm', 'as' => 'passwordForgot']);
+    Route::post('send-mail', ['uses' => 'Backend\AuthController@sendMail', 'as' => 'sendMail']);
+    Route::get('password/reset/{token}', 'Backend\AuthController@showResetForm');
+    Route::post('password/reset', 'Backend\AuthController@resetPassword');
 
-        //Password reset routes
-        Route::get('forgot-password', ['uses' => 'Backend\AuthController@showForgotForm', 'as' => 'passwordForgot']);
-        Route::post('send-mail', ['uses' => 'Backend\AuthController@sendMail', 'as' => 'sendMail']);
-        Route::get('password/reset/{token}', 'Backend\AuthController@showResetForm');
-        Route::post('password/reset', 'Backend\AuthController@resetPassword');
+    Route::resource('dashboard', 'Backend\DashboardController');
 
-        Route::resource('dashboard', 'Backend\DashboardController');
-
-        //Roles manager routes
-        Route::prefix('roles/')->group(function () {
-            Route::get('{roleId}/choosePermission', ['uses' => 'Backend\RolesController@viewChoosePermission',])->name('roles.choosePermission');
-            Route::post('{roleId}/assignRole', ['uses' => 'Backend\RolesController@assignRole',])->name('roles.assignRole');
-        });
-        Route::resource('roles', 'Backend\RolesController');
-
-        //Permissions manager routes
-        Route::resource('permissions', 'Backend\PermissionsController');
-
-        //Users manager routes
-        Route::prefix('users/')->group(function () {
-            Route::put('updateProfile/{userId}', ['uses' => 'Backend\UsersController@updateProfile',])->name('users.updateProfile');
-        });
-        Route::resource('users', 'Backend\UsersController');
-
-        //====Book start=============
-        Route::prefix('books/')->group(function () {
-            Route::get('/delete', [
-                'uses' => 'Backend\BookController@delete',
-            ]);
-            Route::get('/update', [
-                'uses' => 'Backend\BookController@update',
-            ]);
-            Route::post('/update', [
-                'uses' => 'Backend\BookController@update',
-            ]);
-            Route::post('/updateStatus', [
-                'uses' => 'Backend\BookController@updateStatus',
-            ]);
-            Route::post('/getChildCat', [
-                'uses' => 'Backend\BookController@getChildCat',
-            ]);
-        });
-
-        Route::resource('books', 'Backend\BookController');
-
-        //====Book end===============
-
-        //====Category start=============
-        Route::prefix('categories/')->group(function () {
-            Route::get('/delete', [
-                'uses' => 'Backend\CategoryController@delete',
-            ]);
-            Route::get('/update', [
-                'uses' => 'Backend\CategoryController@update',
-            ]);
-            Route::post('/update', [
-                'uses' => 'Backend\CategoryController@update',
-            ]);
-        });
-
-        Route::resource('categories', 'Backend\CategoryController');
-
-        //====Category end===============
-
-        //====Pin start=============
-        Route::prefix('pins/')->group(function () {
-            Route::post('/create', [
-                'uses' => 'Backend\PinController@create',
-            ]);
-        });
-
-        Route::resource('pins', 'Backend\PinController');
-
-        //====Pin end===============
-
-        //====Library start=============
-        Route::prefix('libraries/')->group(function () {
-            Route::get('/delete', [
-                'uses' => 'Backend\LibraryController@delete',
-            ]);
-            Route::get('/update', [
-                'uses' => 'Backend\LibraryController@update',
-            ]);
-            Route::post('/update', [
-                'uses' => 'Backend\LibraryController@update',
-            ]);
-            Route::post('/updateShare', [
-                'uses' => 'Backend\LibraryController@updateShare',
-            ]);
-        });
-
-        Route::resource('libraries', 'Backend\LibraryController');
-
-        //====Library end===============
-
-        //====Rss start=============
-
-        Route::get('rss/delete/{rss_id}', [
-            'as' => 'rss.delete', 'uses' => 'Backend\RssController@delete'
-        ]);
-
-        Route::get('rss/user',[
-            'as' => 'rss.user', 'uses' => 'Backend\RssController@rssUserIndex'
-        ]);
-
-        Route::resource('rss', 'Backend\RssController');
-
-        //====Rss end===============
-
-        //====Discussion start=============
-        Route::prefix('discussions/')->group(function () {
-            Route::get('/delete', [
-                'uses' => 'Backend\DiscussionController@delete',
-            ]);
-            Route::get('/update', [
-                'uses' => 'Backend\DiscussionController@update',
-            ]);
-            Route::post('/update', [
-                'uses' => 'Backend\DiscussionController@update',
-            ]);
-            Route::post('/updateShare', [
-                'uses' => 'Backend\DiscussionController@updateShare',
-            ]);
-        });
-
-        Route::resource('discussions', 'Backend\DiscussionController');
-
-        //====Discussion end===============
-
-        //====Event start=============
-        Route::prefix('events/')->group(function () {
-            Route::get('/delete', [
-                'uses' => 'Backend\EventController@delete',
-            ]);
-            Route::get('/update', [
-                'uses' => 'Backend\EventController@update',
-            ]);
-            Route::post('/update', [
-                'uses' => 'Backend\EventController@update',
-            ]);
-        });
-
-        Route::resource('events', 'Backend\EventController');
-
-        //====Event end===============
-
-        //====Product start=============
-        Route::prefix('products/')->group(function () {
-            Route::get('/delete', [
-                'uses' => 'Backend\ProductController@delete',
-            ]);
-            Route::get('/update', [
-                'uses' => 'Backend\ProductController@update',
-            ]);
-            Route::post('/update', [
-                'uses' => 'Backend\ProductController@update',
-            ]);
-        });
-
-        Route::resource('products', 'Backend\ProductController');
-
-        //====Product end===============
-
-        //====Draft start=============
-        Route::prefix('drafts/')->group(function () {
-            Route::get('/delete', [
-                'uses' => 'Backend\DraftController@delete',
-            ]);
-            Route::get('/update', [
-                'uses' => 'Backend\DraftController@update',
-            ]);
-            Route::post('/update', [
-                'uses' => 'Backend\DraftController@update',
-            ]);
-        });
-
-        Route::resource('drafts', 'Backend\DraftController');
-
-        //====Draft end===============
-
-
-        
-
+    //Roles manager routes
+    Route::prefix('roles/')->group(function () {
+        Route::get('{roleId}/choosePermission', ['uses' => 'Backend\RolesController@viewChoosePermission',])->name('roles.choosePermission');
+        Route::post('{roleId}/assignRole', ['uses' => 'Backend\RolesController@assignRole',])->name('roles.assignRole');
     });
+    Route::resource('roles', 'Backend\RolesController');
+
+    //Permissions manager routes
+    Route::resource('permissions', 'Backend\PermissionsController');
+
+    //Users manager routes
+    Route::prefix('users/')->group(function () {
+        Route::put('updateProfile/{userId}', ['uses' => 'Backend\UsersController@updateProfile',])->name('users.updateProfile');
+    });
+    Route::resource('users', 'Backend\UsersController');
+
+    //====Book start=============
+    Route::prefix('books/')->group(function () {
+        Route::get('/delete', [
+            'uses' => 'Backend\BookController@delete',
+        ]);
+        Route::get('/update', [
+            'uses' => 'Backend\BookController@update',
+        ]);
+        Route::post('/update', [
+            'uses' => 'Backend\BookController@update',
+        ]);
+        Route::post('/updateStatus', [
+            'uses' => 'Backend\BookController@updateStatus',
+        ]);
+        Route::post('/getChildCat', [
+            'uses' => 'Backend\BookController@getChildCat',
+        ]);
+    });
+
+    Route::resource('books', 'Backend\BookController');
+
+    //====Book end===============
+
+    //====Category start=============
+    Route::prefix('categories/')->group(function () {
+        Route::get('/delete', [
+            'uses' => 'Backend\CategoryController@delete',
+        ]);
+        Route::get('/update', [
+            'uses' => 'Backend\CategoryController@update',
+        ]);
+        Route::post('/update', [
+            'uses' => 'Backend\CategoryController@update',
+        ]);
+    });
+
+    Route::resource('categories', 'Backend\CategoryController');
+
+    //====Category end===============
+
+    //====Pin start=============
+    Route::prefix('pins/')->group(function () {
+        Route::post('/create', [
+            'uses' => 'Backend\PinController@create',
+        ]);
+    });
+
+    Route::resource('pins', 'Backend\PinController');
+
+    //====Pin end===============
+
+    //====Library start=============
+    Route::prefix('libraries/')->group(function () {
+        Route::get('/delete', [
+            'uses' => 'Backend\LibraryController@delete',
+        ]);
+        Route::get('/update', [
+            'uses' => 'Backend\LibraryController@update',
+        ]);
+        Route::post('/update', [
+            'uses' => 'Backend\LibraryController@update',
+        ]);
+        Route::post('/updateShare', [
+            'uses' => 'Backend\LibraryController@updateShare',
+        ]);
+    });
+
+    Route::resource('libraries', 'Backend\LibraryController');
+
+    //====Library end===============
+
+    //====Rss start=============
+
+    Route::get('rss/delete/{rss_id}', [
+        'as' => 'rss.delete', 'uses' => 'Backend\RssController@delete'
+    ]);
+
+    Route::get('rss/user',[
+        'as' => 'rss.user', 'uses' => 'Backend\RssController@rssUserIndex'
+    ]);
+
+    Route::resource('rss', 'Backend\RssController');
+
+    //====Rss end===============
+
+    //====Discussion start=============
+    Route::prefix('discussions/')->group(function () {
+        Route::get('/delete', [
+            'uses' => 'Backend\DiscussionController@delete',
+        ]);
+        Route::get('/update', [
+            'uses' => 'Backend\DiscussionController@update',
+        ]);
+        Route::post('/update', [
+            'uses' => 'Backend\DiscussionController@update',
+        ]);
+        Route::post('/updateShare', [
+            'uses' => 'Backend\DiscussionController@updateShare',
+        ]);
+    });
+
+    Route::resource('discussions', 'Backend\DiscussionController');
+
+    //====Discussion end===============
+
+    //====Event start=============
+    Route::prefix('events/')->group(function () {
+        Route::get('/delete', [
+            'uses' => 'Backend\EventController@delete',
+        ]);
+        Route::get('/update', [
+            'uses' => 'Backend\EventController@update',
+        ]);
+        Route::post('/update', [
+            'uses' => 'Backend\EventController@update',
+        ]);
+    });
+
+    Route::resource('events', 'Backend\EventController');
+
+    //====Event end===============
+
+    //====Product start=============
+    Route::prefix('products/')->group(function () {
+        Route::get('/delete', [
+            'uses' => 'Backend\ProductController@delete',
+        ]);
+        Route::get('/update', [
+            'uses' => 'Backend\ProductController@update',
+        ]);
+        Route::post('/update', [
+            'uses' => 'Backend\ProductController@update',
+        ]);
+    });
+
+    Route::resource('products', 'Backend\ProductController');
+
+    //====Product end===============
+
+    //====Draft start=============
+    Route::prefix('drafts/')->group(function () {
+        Route::get('/delete', [
+            'uses' => 'Backend\DraftController@delete',
+        ]);
+        Route::get('/update', [
+            'uses' => 'Backend\DraftController@update',
+        ]);
+        Route::post('/update', [
+            'uses' => 'Backend\DraftController@update',
+        ]);
+    });
+
+    Route::resource('drafts', 'Backend\DraftController');
+
+    //====Draft end===============
+
+
+
+
 });
+
+
+
 
 
