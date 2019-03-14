9127212c3d3e53b97d58cffdd1e0973cc639cc2b
Hung Do <osxsystem2014@gmail.com>
2019-03-14 14:31:48 +0700

    fix UI register

---
+++ app/Http/Controllers/Frontend/BibliothequeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\Category\CategoryRepositoryInterface;
+use App\Repositories\Book\BookRepositoryInterface;
+use Illuminate\Support\Facades\Config;
+use App\Helpers\Envato\Ulities;
+use Elasticsearch\ClientBuilder;
+
+
+class BibliothequeController extends Controller
+{
+    
+}
