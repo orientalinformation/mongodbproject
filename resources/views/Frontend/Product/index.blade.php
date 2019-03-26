01876726352ae6593d74d9beaf0f2875c21324d3
Ngonc <ngonc@dfm-engineering.com>
2019-03-26 15:30:08 +0700

    set modal save keyword

--- resources/views/Frontend/Product/index.blade.php
+++ resources/views/Frontend/Product/index.blade.php
@@ -86,29 +86,7 @@
         </div>
     </div>
 </div>
-<div class="modal fade bd-save-keyword-modal-md" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-md">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">@lang('common.saveSearch')</h4>
-            </div>
-            <div class="modal-body">
-                <form name="frmSaveKeyword" action="{{ route('frontResearchSave') }}">
-                    <div class="form-group rechercher">
-                        <div class="input-group">
-                            <input type="text" name="research_name" class="form-control">
-                            <span class="input-group-btn">
-                                <button id="btn-save-keyword" class="btn btn-primary" type="button"><span class="glyphicon glyphicon-ok" aria-hidden="true">
-                                </span> @lang('common.btnSave')!</button>
-                            </span>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+@include('Frontend.layout.modal-savekeyword')
 @include('Frontend.layout.modal-searchadvance')
 @include('Frontend.layout.modal-tooltip', ['library'])
 @endsection
